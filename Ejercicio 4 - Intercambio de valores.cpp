2f17e81acc7269f73ab0bd9ffdeb2cecec4b65fb
ShhhD20 <142339741+ShhhD20@users.noreply.github.com>
2023-10-04 23:43:15 -0600

    Update Ejercicio 4 - Intercambio de valores.cpp

--- Ejercicio 4 - Intercambio de valores.cpp
+++ Ejercicio 4 - Intercambio de valores.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	int v1, v2, v3;
+	int v1, v2, v3, temp;
 	
 	printf("Ingrese el valor entero 1: ");
 	scanf("%d", &v1);
@@ -9,10 +9,10 @@ int main(){
 	scanf("%d", &v2);
 	printf("Ingrese el valor entero 3: ");
 	scanf("%d", &v3);
-	
-	v2 = v1; 
-	v3 = v2; 
-	v1 = v3;
+	temp = v3;
+	v3 = v2;
+	v2 = v1;
+	v1 = temp;
 	printf(" v1 %d\n v2 %d\n v3 %d", v1, v2, v3);
 	
 	return 0;
